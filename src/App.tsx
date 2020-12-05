08806d14b1e90eecdedb0fd92cebe56c34f3662a
Helena Paixão <hp.helenapaixao@gmail.com>
2020-12-05 12:05:36 -0400

    :stada:feat:add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './styles/global.css'
 import './styles/pages/landing.css'
+import logoImg from './assets/images/Logo.svg';
 
 function App() {
   return (
       <div id="page-landing">
+          <div className="content-wrapper">
+              <img src={logoImg} alt="Logo"/>
+          </div>
          <h1>Hello Bitch </h1>
       </div>
  
